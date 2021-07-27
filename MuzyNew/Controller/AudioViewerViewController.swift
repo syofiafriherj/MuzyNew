ab53fe3ffe509ec665735a38355cbdf722daf8d9
syofiafriherj <syofiafriherj@gmail.com>
2021-07-28 00:09:51 +0700

    1. Adding 2 New Storyboard (RecordingListScreen and AudioViewerScreen) 2. Adding 2 ViewController

---
+++ MuzyNew/Controller/AudioViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AudioViewerViewController.swift
+//  MuzyNew
+//
+//  Created by Syofia Friherdina on 28/07/21.
+//
+
+import UIKit
+
+class AudioViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
